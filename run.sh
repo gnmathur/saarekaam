efc282e2abef3f93d016f352ade222453db37129
Gaurav Mathur <gmathur@gurukul>
2023-10-14 00:46:04 -0700

    Removed JMX prometheus exporter lib
    The prometheus hotspot dependency provides those metrics from within
    the application itself. The java agent is not needed.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn package && java -javaagent:./lib/jmx_prometheus_javaagent-0.20.0.jar=8899:./lib/prometheus_jmx_config.yaml -cp target/saarekaam-1.0-SNAPSHOT-jar-with-dependencies.jar com.gnmathur.saarekaam.Main
+mvn package && java -cp target/saarekaam-1.0-SNAPSHOT-jar-with-dependencies.jar com.gnmathur.saarekaam.Main
